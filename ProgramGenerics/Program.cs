32a92cd21720f1c2819e0efac8fa3fe0d78587c5
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-02 17:24:42 +0530

    Find The Max Value in UC1_FindIntValue

--- ProgramGenerics/Program.cs
+++ ProgramGenerics/Program.cs
@@ -11,9 +11,21 @@ namespace ProgramGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Generics Program");
-
-            FindMaxintValue.MaxValue(10,20,30);
-
+            Console.WriteLine("Choose The Option From Below");
+            Console.WriteLine("1:FindMaxValue\n2:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                     Console.WriteLine("Enter The First Number");
+                     int a = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Enter The Second Number");
+                     int b = Convert.ToInt32(Console.ReadLine());
+                     Console.WriteLine("Enter The Third Number");
+                     int c = Convert.ToInt32(Console.ReadLine());
+                     FindMaxintValue.MaxValue(a, b, c);
+                     break;
+            }
 
             Console.ReadLine();
         }
